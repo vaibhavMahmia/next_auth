dc294219a25a7155eef04dd818777ecc05d965ec
vaibhavM <vaibhav@kali.kali>
2025-07-10 19:22:27 +0530

    user auth_action: error handling for password and email

---
+++ actions/auth_actions.js
@@ -0,0 +1,12 @@
+'use server';
+
+export const signup = async (prevState, formData) => {
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    let errors = {};
+    if(!email.includes('@')) errors.email = 'Please enter a valid email address.';
+    if(password.trim().length < 8) errors.password = 'Password must be at least 8 characters long.';
+
+    if(Object.keys(errors).length > 0) return { errors };
+}
---
+++ components/auth-form.js
@@ -0,0 +1,33 @@
+'use client';
+import { signup } from '@/actions/auth_actions';
+import Link from 'next/link';
+import { useActionState } from 'react';
+
+export const AuthForm = () => {
+  const [formState, formAction] = useActionState(signup, {});
+
+  return <form id="auth-form" action={formAction}>
+    <div>
+      <img src="/images/auth-icon.jpg" alt="A lock icon" />
+    </div>
+    <p>
+      <label htmlFor="email">Email</label>
+      <input type="email" name="email" id="email" />
+    </p>
+    <p>
+      <label htmlFor="password">Password</label>
+      <input type="password" name="password" id="password" />
+    </p>
+    {formState.errors && <ul id='form-errors'>
+      {Object.keys(formState.errors).map((error) => <li key={error}>{formState.errors[error]}</li>)}
+    </ul>}
+    <p>
+      <button type="submit">
+        Create Account
+      </button>
+    </p>
+    <p>
+      <Link href="/">Login with existing account.</Link>
+    </p>
+  </form>;
+}
